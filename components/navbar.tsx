ccf1de9212c4fe6a588ffc149628b4c4ffdda0c7
Matheus Bertemes <mathbertemes@gmail.com>
2024-04-15 20:13:56 -0700

    feat: product page entity

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "@/components/navbar-actions";
 
-export const revalidate = 0;
+export const revalidate =0;
 
 const Navbar = async () => {
   const categories = await getCategories()
