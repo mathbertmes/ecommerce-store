3e6d415bf47e48a6c4cf535ba5b176fca4d3a453
Matheus Bertemes <mathbertemes@gmail.com>
2024-06-04 13:11:20 -0700

    feat: mobile menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "@/components/navbar-actions";
+import { Menu } from "lucide-react";
 
 export const revalidate =0;
 
@@ -12,10 +13,15 @@ const Navbar = async () => {
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <div className="md:hidden">
+            <Menu />
+          </div>
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-          <MainNav data={categories}/>
+          <div className="hidden md:block ">
+            <MainNav data={categories}/>  
+          </div>                        
           <NavbarActions />
         </div>
       </Container>
