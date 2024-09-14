9d882a5ed852aaefc376863685a264dda5c300e9
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-14 00:35:44 -0700

    refactor: billboard sale

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,23 +1,39 @@
+"use client"
 import { Billboard as BillboardType } from "@/types";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BillboardProps{
-  data: BillboardType;
+  data: string;
+  saleBanner?: boolean
 }
 
 const Billboard: React.FC<BillboardProps> = ({
-  data
+  data,
+  saleBanner
 }) => {
+
+  const router = useRouter()
+
+  const handleClickSaleBanner = () => {
+    if(saleBanner) {
+      router.push(`/sale`)
+    }
+    
+  }
   return(
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div 
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
-        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        className="rounded-xl relative w-full md:aspect-[2.4/1] overflow-hidden bg-cover"
       >
-        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+        <Image onClick={handleClickSaleBanner} src={data} width={500} height={900} objectFit="contain" alt="Banner image">
+
+        </Image>
+        {/* <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
