225a5be917fe21ab07aec2490a693e92c03ba363
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-14 00:36:15 -0700

    refactor: homepage billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,25 @@
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
+import getSale from "@/actions/get-sale";
 import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+
 
 export const revalidate =0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("6ed76ac5-6e75-43df-b9d6-75e4504a5154");
+  const sale = await getSale()
+
+
   return(
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard}/>
+        {/* <Billboard data={billboard}/> */}
+        {sale && (
+            <Billboard saleBanner data={sale.imageUrl!}/>
+        )}
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products}/>
         </div>
