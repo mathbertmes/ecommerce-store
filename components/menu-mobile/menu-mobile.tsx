9b7abe6ac6dedc9f0c5ec07a428d9692fe854d5e
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-25 16:02:45 -0700

    feat: mobile menu

--- components/menu-mobile/menu-mobile.tsx
+++ components/menu-mobile/menu-mobile.tsx
@@ -1,10 +1,19 @@
+import useMobileMenu from "@/hooks/use-mobile-menu";
+import ModalMenuMobile from "./modal-menu-mobile";
 
 
 const MenuMobile = () => {
+  const previewModal = useMobileMenu()
+
   return(
-    <div>
-      ola
-    </div>
+    <ModalMenuMobile
+      open={previewModal.isOpen}
+      onClose={previewModal.onClose}
+    >
+      <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+        <h1>testt</h1>
+      </div>
+    </ModalMenuMobile>
   )
 }
 
