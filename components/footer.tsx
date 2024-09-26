e55749843741b74cb7843a1d1442d294b61cedac
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-26 02:21:47 -0700

    feat: whatsapp

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin } from "lucide-react";
+import { Github, Linkedin, MessageCircle } from "lucide-react";
 import Link from "next/link";
 
 const Footer = () => {
@@ -6,9 +6,10 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2024 FakeStoreName, Inc. All rights reserved.
+          &copy; 2024 FakeStoreName, Inc. All rights reserved. Developed by <b>Matheus Bertemes</b>.
         </p>
-        <div className="flex flex-col items-center mt-4 gap-2">
+        <div className="flex items-center justify-center mt-4">
+          <div className="flex flex-col items-start  gap-2">
           <div className="flex gap-4">
           <Linkedin />
           <Link href="https://www.linkedin.com/in/matheus-bertemes-96aa97187/">Linkedin</Link>
@@ -17,7 +18,11 @@ const Footer = () => {
           <Github />
           <Link href="https://github.com/mathbertmes">Github</Link>
           </div>
-          
+          <div className="flex gap-4">
+          <MessageCircle />
+          <Link href="https://wa.me/5547988494549">WhatsApp</Link>
+          </div>
+          </div>
         </div>
       </div>
     </footer>
